b135dd74cd9f028ef65f8914a847b2c9af11a46f
Jeremi Torój <jeremi.toroj@icloud.com>
2025-11-30 03:11:41 +0100

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website/', // repo name here
+  base: '/website/', // repo name
 })
 
